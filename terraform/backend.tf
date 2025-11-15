4b7fd1572586335e1575a2e4ae6fab8cd6d9f4ec
Amadidominic15 <amadidominic15@gmail.com>
2025-11-15 14:14:01 +0100

    add terraform

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" { 
+    bucket         = "kanyuy-frontend-static-2025"
+    key            = "Ecocast/terraform.tfstate"
+    region         = "af-south-1"
+    use_lockfile   = true
+  }
+}
